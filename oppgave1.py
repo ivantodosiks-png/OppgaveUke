ca50cdbfef8f3131f96a0a03300a294dfaf11c66
ivantodosiks-png <ivan.todosiks@gmail.com>
2026-03-20 08:50:52 +0100

    Implement initial project structure and add essential files

---
+++ oppgave1.py
@@ -0,0 +1,10 @@
+
+age = int(input("Hvor gammel er du?"))
+
+if age >=18:
+    print("Du er helt sommerlig!")
+else:
+    print("Du er mindreårig!")
+
+
+    
---
+++ quiz.py
@@ -0,0 +1,39 @@
+print("Ты готов к вопросам?!")
+print("Поехали!")
+poeng = 0
+
+math1 = int(input("Сколько будет 2+2:"))
+
+if math1 == 4:
+    poeng = poeng +1
+    print("Riktig!")
+    print("poeng +1")
+else:
+    print("Feil!")
+
+
+
+
+
+    math1 = int(input("Сколько будет 10*10:"))
+
+if math1 == 100:
+    poeng = poeng +1
+    print("Riktig!")
+    print("poeng +1")
+else:
+    print("Feil!")
+
+print("game over")
+print(poeng)
+
+
+
+
+
+
+
+
+
+
+
